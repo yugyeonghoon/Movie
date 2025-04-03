dfe1e1dfb6055302b800d3e532bedf8a8557443d
kkk <yuh3298@naver.com>
2025-04-03 12:49:54 +0900

    영화 정보저장 아마 최종

---
+++ Movie/python/01.movie_date_final.py
@@ -0,0 +1,88 @@
+import requests
+import pandas as pd
+import json
+import re
+
+url = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=H2W8Y70VM9R38UQ32FVL&startCount1&listCount=500"
+result = []
+list_count = 500
+
+for i in range(0, 224) :
+
+    start_count = i * list_count
+    #0 * 500 -> 0 -> 0페이지부터 500개 0 ~ 499개
+    #1 * 500 -> 500 -> 500페이지부터 500개 500 ~ 999
+    #2 * 500 -> 1000 -> 1000페이지부터 500개 1000 ~ 1499
+
+    url = f"http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=H2W8Y70VM9R38UQ32FVL&startCount={start_count}&listCount={list_count}"
+
+    print(url)
+    response = requests.get(url).text
+    response = response.replace("\n", "").replace("\r", "")
+    response = re.sub(r"[\x00-\x1F\x7F]", "", response)
+    #x00x1f ~ x00x7f
+
+    response = json.loads(response)
+    
+    datas = response["Data"][0]["Result"]
+
+    for data in datas:
+        #print(data["DOCID"])
+        #print(data["title"])
+
+        # directors = data["directors"]["director"]
+
+        # 감독 = []
+        
+        # for director in directors:
+        #     감독.append(director["directorNm"])
+        
+        # 감독 = " ".join(감독)
+        # print(감독)
+
+        directors = " ".join([director["directorNm"] for director in data["directors"]["director"]])
+
+        actors = " ".join([actor["actorNm"] for actor in data["actors"]["actor"][:5]])
+
+        nation = data["nation"]
+
+        genre = data["genre"]
+
+        plots = data["plots"]["plot"][0]["plotText"]
+
+        repRlsDate = data["repRlsDate"]
+
+        posters = data["posters"]
+        try :
+            poster = posters.split("|")[0]
+        except :
+            poster = ""
+
+        runtime = data["runtime"]
+
+        ratingGrade = data["ratings"]["rating"][0]["ratingGrade"]
+        try :
+            ratingGrade = ratingGrade.split("||")[0]
+        except :
+            ratingGrade = ""
+
+        dict = {
+            "docid" : data["DOCID"],
+            "title" : data["title"],
+            "directors" : directors,
+            "actors" : actors,
+            "nation" : nation,
+            "genre" : genre,
+            "plots" : plots,
+            "repRlsDate" : repRlsDate,
+            "poster" : poster,
+            "runtime" : runtime,
+            "ratingGrade" : ratingGrade
+        }
+
+        result.append(dict)
+
+df = pd.DataFrame(result)
+
+print(df)
+df.to_csv("01.all_movie.csv", index=False)
---
+++ Movie/python/02.process.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+df = pd.read_csv("01.all_movie.csv")
+
+df["title"] = df["title"].str.strip()
+
+print(df.columns)
+
+print(df["plots"])
+
+df.dropna(subset=["plots"], inplace=True)
+
+print(df.info())
+
+df["poster"].fillna("aa.jpg", inplace=True)
+print(df.info())
+
+print(df["genre"] == "에로")
+
+df["genre"].fillna("자료없음", inplace=True)
+
+df.drop(df[df["genre"] == "에로"].index, inplace=True)
+
+df.drop(df[df["genre"].str.contains("에로")].index, inplace=True)
+
+df["actors"].fillna("자료없음", inplace=True)
+
+df["directors"].fillna("자료없음", inplace=True)
+
+df["nation"].fillna("자료없음", inplace=True)
+
+df["runtime"].fillna(0, inplace=True)
+
+df["ratingGrade"].fillna("자료없음", inplace=True)
+
+df["repRlsDate"].fillna(20250403, inplace=True)
+
+df["repRlsDate"] = df["repRlsDate"].astype(int)
+
+df.drop(df[df["runtime"] < 40].index, inplace=True)
+
+df["runtime"] = df["runtime"].astype(int)
+
+print(df.info())
+
+df["rating"] = "0"
+
+df.to_csv("02.edit_movie.csv", index=False)
