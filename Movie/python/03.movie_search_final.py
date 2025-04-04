9880313e9e3fab523a35488728d0d8dcc31b2415
kkk <yuh3298@naver.com>
2025-04-04 12:48:00 +0900

    평점 수정

--- Movie/python/03.movie_search_final.py
+++ Movie/python/03.movie_search_final.py
@@ -15,8 +15,13 @@ print(df["title"])
 rvs_list = []
 print(df)
 
-for i, t in enumerate(df["title"][:10]):
+start_index = 0
+last_index = 10000
+
+for i, t in enumerate(df["title"][start_index:last_index]):
   title = f"영화 {t} 관람평"
+
+  index = i + start_index
 
   print(t)
   driver.get(f"https://search.naver.com/search.naver?ssc=tab.nx.all&where=nexearch&query={title}&sm=tab_org&qvt=0")
@@ -26,9 +31,9 @@ for i, t in enumerate(df["title"][:10]):
   try:
     rvs = driver.find_element(By.XPATH, '//*[@class="main_pack"]/div[3]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/ul/li[1]/div[1]/div[1]/span[1]')
     print(rvs.text)
-    df.loc[i, "rating"] = rvs.text
+    df.loc[index, "rating"] = rvs.text
     time.sleep(2)
   except Exception as e:
     print(e)
   
-df.to_csv("03.movie_with_rating_ex.csv", index=False)
+df.to_csv("03.movie_with_rating_1.csv", index=False)
