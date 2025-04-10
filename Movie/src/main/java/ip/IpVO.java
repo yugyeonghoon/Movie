c543a9d6eee6326be670acb6ae94e9671013ddc5
ykh <ykh@naver.com>
2025-04-10 10:04:36 +0900

    ip dao vo

---
+++ Movie/src/main/java/ip/IpVO.java
@@ -0,0 +1,27 @@
+package ip;
+
+public class IpVO {
+	private String ip;
+	private String menu_id;
+	private String loginTime;
+	
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getMenu_id() {
+		return menu_id;
+	}
+	public void setMenu_id(String menu_id) {
+		this.menu_id = menu_id;
+	}
+	public String getLoginTime() {
+		return loginTime;
+	}
+	public void setLoginTime(String loginTime) {
+		this.loginTime = loginTime;
+	}
+	
+}
